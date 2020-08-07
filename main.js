4442d6391a3b235acdc29090ca900c7ae145656f
ishvar99 <ishanvarshney99@gmail.com>
2020-08-07 13:25:30 +0530

    keeping track of environments and platforms.

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 const { app, BrowserWindow } = require("electron")
+process.env.NODE_ENV = "development"
+const isDev = process.env.NODE_ENV !== "production" ? true : false
+const isMac = process.platform === "darwin" ? true : false
 let mainWindow
 function createMainWindow() {
   mainWindow = new BrowserWindow({
@@ -6,8 +9,18 @@ function createMainWindow() {
     width: 500,
     height: 600,
     icon: "./assets/icons/icon_128x128.png",
+    resizable: isDev, //if development, resizable is true otherwise false
   })
   mainWindow.loadFile("./app/index.html")
 }
-
+app.on("window-all-closed", () => {
+  if (!isMac) {
+    app.quit()
+  }
+})
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createMainWindow()
+  }
+})
 app.on("ready", createMainWindow)
