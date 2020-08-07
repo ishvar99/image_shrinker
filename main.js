f3a3596fdb3beed7bfcd7ccaff94bd469cfccb64
ishvar99 <ishanvarshney99@gmail.com>
2020-08-07 14:21:11 +0530

    setup global shortcuts and accelerator

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, Menu } = require("electron")
+const { app, BrowserWindow, Menu, globalShortcut } = require("electron")
 process.env.NODE_ENV = "development"
 const isDev = process.env.NODE_ENV !== "production" ? true : false
 const isMac = process.platform === "darwin" ? true : false
@@ -27,6 +27,7 @@ const menu = [
     submenu: [
       {
         label: "quit",
+        accelerator: "CmdOrCtrl+W",
         click: app.quit,
       },
     ],
@@ -44,6 +45,10 @@ app.on("activate", () => {
 })
 app.on("ready", () => {
   createMainWindow()
+  globalShortcut.register("CmdOrCtrl+R", () => mainWindow.reload())
+  globalShortcut.register(isMac ? "Cmd+Alt+I" : "Ctrl+Shift+I", () =>
+    mainWindow.toggleDevTools()
+  )
   const mainMenu = Menu.buildFromTemplate(menu)
   Menu.setApplicationMenu(mainMenu)
   app.on("closed", () => (mainWindow = null))
