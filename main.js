9dae46640403edcbae3c33dad59e5697242ac28a
ishvar99 <ishanvarshney99@gmail.com>
2020-08-07 13:41:22 +0530

    using electron-reload to reload changes in dev env

--- main.js
+++ main.js
@@ -2,13 +2,20 @@ const { app, BrowserWindow } = require("electron")
 process.env.NODE_ENV = "development"
 const isDev = process.env.NODE_ENV !== "production" ? true : false
 const isMac = process.platform === "darwin" ? true : false
+const path = require("path")
+if (isDev) {
+  require("electron-reload")(__dirname, {
+    electron: path.join(__dirname, "node_modules", ".bin", "electron.cmd"),
+    hardResetMethod: "exit",
+  })
+}
 let mainWindow
 function createMainWindow() {
   mainWindow = new BrowserWindow({
     title: "Image Shrinker",
-    width: 500,
+    width: 400,
     height: 600,
-    icon: "./assets/icons/icon_128x128.png",
+    // icon: "./assets/icons/icon_128x128.png",
     resizable: isDev, //if development, resizable is true otherwise false
   })
   mainWindow.loadFile("./app/index.html")
